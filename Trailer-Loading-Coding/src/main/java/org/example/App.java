d2bcac1069f562ded3add391e756f759dca71d9f
mwrowan89 <mwrowan89@gmail.com>
2024-11-08 10:51:34 -0500

    CLI working with inputs and parsing data

--- Trailer-Loading-Coding/src/main/java/org/example/App.java
+++ Trailer-Loading-Coding/src/main/java/org/example/App.java
@@ -6,14 +6,28 @@ import java.util.Scanner;
 public class App
 {
     public static void main( String[] args ) {
+        String shape = "";
+        String position = "";
 
         Scanner input = new Scanner(System.in);
-        System.out.print("What comes after five?  ");  // prints out a message
+        System.out.println("Welcome To The Trailer-Loader-Coder!");
+        System.out.print("Please enter a number and shape: ");
         String userInput = input.nextLine();
 
-        if(userInput.equalsIgnoreCase("six")) {
+        position = userInput.substring(0,1).toUpperCase();
+        shape = userInput.substring(1).toUpperCase();
+
+        if(position.equalsIgnoreCase("") || shape.equalsIgnoreCase("")){
+            System.out.println("Invalid Entry.");
+            System.exit(0);
+        }
+
+        if(position.equalsIgnoreCase("6")) {
             System.out.println("SIX!");
         } else
             System.out.println("Not Six");
+
+        System.out.println("Position: " + position);
+        System.out.println("Shape: " + shape);
     }
 }
