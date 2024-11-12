d5d26a18ae23fa582c1d5532c28e8c313304f15b
mwrowan89 <mwrowan89@gmail.com>
2024-11-12 14:04:53 -0500

    Moved CLI commands to main app

--- Trailer-Loading-Coding/src/main/java/org/example/Program.java
+++ Trailer-Loading-Coding/src/main/java/org/example/Program.java
@@ -10,45 +10,11 @@ public class Program {
     public static int cols = 10;
     public static String[][] truck = buildTruck(rows, cols);
 
+    public Program() {
+
+    }
+
     public static void program() {
-        String shape;
-        String position;
-        Map<String, String> shapeMap = new HashMap<>();
-        boolean inputs = true;
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Welcome To The Trailer-Loader-Coder!");
-
-        while (inputs) {
-            System.out.print("Please enter a number and shape: ");
-            String userInput = input.nextLine();
-
-            position = userInput.substring(0, 1).toUpperCase();
-            shape = userInput.substring(1).toUpperCase();
-
-            if (position.equalsIgnoreCase("") || shape.equalsIgnoreCase("")) {
-                System.out.println("Invalid Entry.");
-                System.out.println("Provide a valid entry.");
-                continue;
-            }
-            if (userInput.length() > 2) {
-                System.out.println("Invalid Entry.");
-                System.out.println("Please provide a valid entry.");
-                continue;
-            }
-
-            shapeMap.put(position, shape);
-
-            System.out.println("Position: " + position);
-            System.out.println("Shape: " + shape);
-            System.out.println(shapeMap);
-            System.out.println("Add another shape? (Y/N)");
-            String userResponse = input.nextLine();
-
-            if (userResponse.equalsIgnoreCase("N")) {
-                inputs = false;
-            }
-        }
     }
 
     private static String[][] buildTruck(int rows, int cols) {
