5f54e4e37386bb9fa989f49f8bec6c94cbbb2e55
mwrowan89 <mwrowan89@gmail.com>
2024-11-18 15:03:45 -0500

    Code stable and working again

--- Trailer-Loading-Coding/src/main/java/org/example/Truck.java
+++ Trailer-Loading-Coding/src/main/java/org/example/Truck.java
@@ -44,20 +44,20 @@ public class Truck {
 
         System.out.println("Pos: " + Arrays.deepToString(currentShape));
         System.out.println("Len: " + currentShape.length);
-//
-//        for(int i = 0; i < currentShape.length; i++){
-//            for(int j = 0; j < currentShape[j].length; j++) {
-//                if(currentShape[i][j] == 1) {
-//                    truck[currentShape[i][j]][position] = "\u001B[94m" + "7" + "\u001B[0m";
-//                }
-//                System.out.println("Current shape val: " + (currentShape[i][j]));
-//                System.out.println(Arrays.toString(currentShape[j]));
-//                System.out.println("Current pos: " + position);
-//                printTruck();
-//                System.out.println();
-//            }
-//            position++;
-//        }
+
+        for(int i = 0; i < currentShape.length; i++){
+            for(int j = 0; j < currentShape[j].length; j++) {
+                if(currentShape[i][j] == 1) {
+                    truck[currentShape[i][j]][position] = "\u001B[94m" + "7" + "\u001B[0m";
+                }
+                System.out.println("Current shape val: " + (currentShape[i][j]));
+                System.out.println(Arrays.toString(currentShape[j]));
+                System.out.println("Current pos: " + position);
+                printTruck();
+                System.out.println();
+            }
+            position++;
+        }
     }
 
     }
