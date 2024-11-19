4ac4b70cb90dd703a55af12808a96daedfa4cc8c
mwrowan89 <mwrowan89@gmail.com>
2024-11-19 10:04:45 -0500

    Corrected the L shape

--- Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
+++ Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
@@ -13,7 +13,7 @@ public class Shapes {
         shapes.put("I", new int[][]{{1},{1},{1},{1}});
         shapes.put("S", new int[][]{{0,1},{1,1},{1,0}});
         shapes.put("Z", new int[][]{{1,0},{1,1},{0,1}});
-        shapes.put("L", new int[][]{{1,1},{1,0},{1,0},{1,0}});
+        shapes.put("L", new int[][]{{1,1},{0,1},{0,1},{0,1}});
         shapes.put("J", new int[][]{{1,1},{0,1},{0,1},{0,1}});
         shapes.put("T", new int[][]{{0, 1, 0}, {1, 1, 1}});
 
