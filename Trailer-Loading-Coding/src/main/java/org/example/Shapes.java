2d24d23b29305f5d8b8b7e89112f3c9ed696c7d1
mwrowan89 <mwrowan89@gmail.com>
2024-11-19 10:03:34 -0500

    added shape corrections and todos

--- Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
+++ Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
@@ -7,6 +7,8 @@ public class Shapes {
     public Map<String, int[][]> shapes = new HashMap<>();;
 
     public Map<String, int[][]> getShapes() {
+        //TODO fix shape sizing and spacing;
+        //TODO S, Z, L, J
         shapes.put("O", new int[][]{{1, 1}, {1, 1}});
         shapes.put("I", new int[][]{{1},{1},{1},{1}});
         shapes.put("S", new int[][]{{0,1},{1,1},{1,0}});
