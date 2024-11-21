5f0f837685e5165c5341fe6c51d8be89ec42fc7d
mwrowan89 <mwrowan89@gmail.com>
2024-11-21 11:16:03 -0500

    Added new shape X

--- Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
+++ Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
@@ -14,6 +14,7 @@ public class Shapes {
         shapes.put("L", new int[][]{{1,1},{0,1},{0,1},{0,1}});
         shapes.put("J", new int[][]{{1,1},{1,0},{1,0},{1,0}});
         shapes.put("T", new int[][]{{0, 1, 0}, {1, 1, 1}});
+        shapes.put("X", new int[][]{{1, 0, 1}, {0, 1, 0}, {1, 0, 1}});
 
         return shapes;
     }
