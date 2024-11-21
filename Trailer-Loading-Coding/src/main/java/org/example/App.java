0c441782a500220c8851ef1f3d99b0b4ce5cf140
mwrowan89 <mwrowan89@gmail.com>
2024-11-21 11:09:24 -0500

    Fixed issue of incorrect inputs

--- Trailer-Loading-Coding/src/main/java/org/example/App.java
+++ Trailer-Loading-Coding/src/main/java/org/example/App.java
@@ -14,16 +14,15 @@ public class App {
         String shape;
         String position;
         boolean inputs = true;
-
         Map<String, int[][]> shapeMap = new HashMap<>(shapes.getShapes());
-
 
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To The Trailer-Loader-Coder!");
 
         while (inputs) {
-            System.out.print("Enter input (x-coordinate (0-9) and a shape (O, I, S, Z, L, J, T)): ");
+            System.out.println("Enter input (x-coordinate (0-9) and a shape (O, I, S, Z, L, J, T)): ");
             System.out.println("Separate inputs with a single comma.");
+            System.out.print("Inputs: ");
             String userInput = input.nextLine();
 
             if(userInput.equalsIgnoreCase("")){
@@ -38,7 +37,7 @@ public class App {
                 part = part.trim();
                 position = part.substring(0, 1).toUpperCase();
                 shape = part.substring(1).toUpperCase();
-                if (userInput.substring(0, 1).equalsIgnoreCase(" ") || userInput.substring(1).equalsIgnoreCase("")) {
+                if (part.substring(0, 1).equalsIgnoreCase(" ") || part.substring(1).equalsIgnoreCase("")) {
                     System.out.println("Invalid Entry.");
                     System.out.println("Provide a valid entry.");
                     continue;
@@ -48,7 +47,7 @@ public class App {
                     System.out.println("Provide a valid entry.");
                     continue;
                 }
-                if (userInput.length() < 2) {
+                if (part.length() < 2) {
                     System.out.println("Invalid Entry.");
                     System.out.println("Please provide a valid entry.");
                     continue;
