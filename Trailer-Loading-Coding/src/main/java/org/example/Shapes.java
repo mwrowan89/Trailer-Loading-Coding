357abf1b611354a87b2cdb354d0fe06b64348c2e
mwrowan89 <mwrowan89@gmail.com>
2024-11-25 11:01:58 -0500

    Added comments to Shape class

--- Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
+++ Trailer-Loading-Coding/src/main/java/org/example/Shapes.java
@@ -7,6 +7,8 @@ public class Shapes {
     public Map<String, int[][]> shapes = new HashMap<>();
 
     public Map<String, int[][]> getShapes() {
+        //Initialization of shape definitions and sizing
+
         shapes.put("O", new int[][]{{1, 1}, {1, 1}});
         shapes.put("I", new int[][]{{1},{1},{1},{1}});
         shapes.put("S", new int[][]{{1,1,0},{0,1,1}});
