7eb90922d54858ba6a6cffaab16c10407258b325
mwrowan89 <mwrowan89@gmail.com>
2024-11-25 10:55:03 -0500

    Added comments to App class

--- Trailer-Loading-Coding/src/main/java/org/example/App.java
+++ Trailer-Loading-Coding/src/main/java/org/example/App.java
@@ -33,48 +33,62 @@ public class App {
                 System.out.println("Provide a valid entry.");
                 continue;
             }
+            //Accepts user inputs
 
             parts = userInput.split(",");
 
+            //Deciphers user inputs
+
             for(String part : parts) {
+                //Interprets individual user inputs
                 part = part.trim();
                 position = part.substring(0, 1);
                 shape = part.substring(1).toUpperCase();
+                //Parses parts of user input
+
                 if (part.substring(0, 1).equalsIgnoreCase(" ") || part.substring(1).equalsIgnoreCase(" ")) {
                     System.out.println("Invalid Entry.");
                     System.out.println("Provide a valid entry.");
                     continue;
+                    //Checks for empty user input
                 }
                 if(!shapeMap.containsKey(shape)){
                     System.out.println("Invalid Entry: " + part);
                     System.out.println("Provide a valid entry.");
                     continue;
+                    //Checks for invalid shape entry
                 }
                 if (part.length() < 2) {
                     System.out.println("Invalid Entry: " + part);
                     System.out.println("Please provide a valid entry.");
                     continue;
+                    //Checks for proper input length
                 }
                 if(Character.getNumericValue(position.charAt(0)) > 10){
                     System.out.println("Invalid Entry: " + part);
                     System.out.println("Please provide a valid entry.");
                     continue;
+                    //Checks that user position input is less than 10
                 }
+
+                //Adds shape using addShape function within Truck class
                 truck.addShape(Integer.parseInt(position), shape);
             }
 
                 truck.printTruck();
                 System.out.println("Max Y: " + truck.findMaxY());
+                //Prints results from Truck class
 
 
 
                 System.out.println();
                 System.out.println("Add another shape? (Y/N)");
                 String userResponse = input.nextLine();
-
+                //Allows user to continue inputs
 
                 if (userResponse.equalsIgnoreCase("N")) {
                     inputs = false;
+                    //Terminates the program if user selects No
                 }
         }
         }
